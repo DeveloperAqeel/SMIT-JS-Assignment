e6333884a39c3345e5f86cb3cc94b1707f9d3ba5
Developer Aqeel <aqeel471860438@gmail.com>
2025-09-01 13:43:42 -0700

    js chapter 6 to 11 updating

---
+++ scriptchp 6 to 9.js
@@ -0,0 +1,6 @@
+// my number
+let num = "10";
+// my calculation
+alert("my value is 10")
+let cal1 = alert("my value is updated: " + ++num) ; // updated now 11
+let cal2 = alert("Now my value is: " + cal1++ )
